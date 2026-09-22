2ae041111a850f1224f8905c1c6e580e5460e21f
Tsubasa Kubo <72246197+nepia-infinity@users.noreply.github.com>
2026-09-22 20:36:13 +0900

    ManifestのDatastore構成を元に戻す

--- manifest.ts
+++ manifest.ts
@@ -1,5 +1,5 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
-import SlackUserProfilesDatastore from "./datastores/slack_user_profiles.ts";
+import UserSettingsDatastore from "./datastores/user_settings.ts";
 import { PostDailyReflectionPromptFunction } from "./functions/post_daily_reflection_prompt.ts";
 import { DailyReflectionWorkflow } from "./workflows/daily_reflection_workflow.ts";
 
@@ -9,7 +9,7 @@ export default Manifest({
   icon: "assets/default_new_app_icon.png",
   functions: [PostDailyReflectionPromptFunction],
   workflows: [DailyReflectionWorkflow],
-  datastores: [SlackUserProfilesDatastore],
+  datastores: [UserSettingsDatastore],
   outgoingDomains: [],
   botScopes: [
     "commands",
