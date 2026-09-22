8f900ee4dab67fc74c15aa4cc68692f48e2ad6da
Tsubasa Kubo <72246197+nepia-infinity@users.noreply.github.com>
2026-09-22 19:49:22 +0900

    チャンネル入力をワークフローから削除

--- workflows/daily_reflection_workflow.ts
+++ workflows/daily_reflection_workflow.ts
@@ -1,23 +1,16 @@
-import { DefineWorkflow, Schema } from "deno-slack-sdk/mod.ts";
+import { DefineWorkflow } from "deno-slack-sdk/mod.ts";
 import { PostDailyReflectionPromptFunction } from "../functions/post_daily_reflection_prompt.ts";
 
 export const DailyReflectionWorkflow = DefineWorkflow({
   callback_id: "daily_reflection_workflow",
   title: "毎日の出来事の振り返り",
-  description: "毎朝、出来事を記録するための案内を投稿します",
+  description: "毎朝、出来事を記録するための案内をDMに投稿します",
   input_parameters: {
-    properties: {
-      channel: {
-        type: Schema.slack.types.channel_id,
-        description: "振り返りメッセージの投稿先チャンネル",
-      },
-    },
-    required: ["channel"],
+    properties: {},
+    required: [],
   },
 });
 
-DailyReflectionWorkflow.addStep(PostDailyReflectionPromptFunction, {
-  channel: DailyReflectionWorkflow.inputs.channel,
-});
+DailyReflectionWorkflow.addStep(PostDailyReflectionPromptFunction, {});
 
 export default DailyReflectionWorkflow;
